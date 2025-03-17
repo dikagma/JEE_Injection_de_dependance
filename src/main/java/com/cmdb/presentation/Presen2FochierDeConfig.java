0971791cf4a50baeae6b993f00e9fed44c3a5520
unknown <dikagma@gmail.com>
2025-03-17 09:40:09 +0100

    version base de donnees

--- src/main/java/com/cmdb/presentation/Presen2FochierDeConfig.java
+++ src/main/java/com/cmdb/presentation/Presen2FochierDeConfig.java
@@ -1,4 +1,34 @@
 package com.cmdb.presentation;
 
+import com.cmdb.dao.IDao;
+import com.cmdb.metier.IMetier;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Scanner;
+
 public class Presen2FochierDeConfig {
+   public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+       Scanner scanner = new Scanner(new File("config.txt"));
+       String daoClassName = scanner.nextLine();
+       //instantiation dynamique
+       Class cDao = Class.forName(daoClassName);
+
+       IDao dao= (IDao) cDao.newInstance(); //si je veux utiliser un constructeur sans parametre
+
+
+      /* System.out.println( dao.getData());
+       System.out.println("test de l'instantiation dynamique  reussi ");*/
+
+       String metierClassName = scanner.nextLine();
+       //instantiation dynamique
+       Class cMetier = Class.forName(metierClassName);
+       IMetier metier= (IMetier) cMetier.getConstructor(IDao.class).newInstance(dao);
+
+       System.out.println("Res " + metier.calcul());
+       System.out.println("la fermeture a la modification de la couche presentation a reussie ");
+
+
+   }
 }
